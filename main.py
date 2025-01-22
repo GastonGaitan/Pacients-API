ef60865e5c56c534f3164c30c43e6fa234f5d0b8
Gastón <gaston-gaitan@hotmail.com>
2025-01-22 00:40:16 -0300

    new endpoint create pacient

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+from pydantic import BaseModel, EmailStr
 
 app = FastAPI()
+
+class Pacient(BaseModel):
+    name: str
+    phone_number: str
+    email: EmailStr
+    document_picture: str
 
 pacients = [
     {
@@ -22,7 +29,7 @@ pacients = [
         "document_picture": "doc_gaston_gaitan_3487235569.jpg"
     },
     {
-        "name": "Vicente pertossi",
+        "name": "Vicente ismael",
         "phone_number": "34872355629",
         "email": "nicolas-capdepon@hotmail.com",
         "document_picture": "doc_nico_capde_3487235569.jpg"
@@ -37,3 +44,8 @@ async def show_all_pacients():
 async def filter_pacients(key: str, value: str):
     filtered_pacients = [pacient for pacient in pacients if value.lower() in pacient.get(key, "").lower()]
     return filtered_pacients
+
+@app.post("/create_pacient")
+async def create_pacient(pacient: Pacient):
+    pacients.append(pacient.model_dump())
+    return {"message": "Pacient created successfully"}
