b6767ca45a2cce9f2108c47a312a4d88f17bac68
Gastón <gaston-gaitan@hotmail.com>
2025-01-22 00:19:03 -0300

    adding endpoint to filter pacients by key value

--- main.py
+++ main.py
@@ -15,8 +15,25 @@ pacients = [
         "email": "nicolas-capdepon@hotmail.com",
         "document_picture": "doc_nico_capde_3487235569.jpg"
     },
+    {
+        "name": "Gaston Gaitan",
+        "phone_number": "3487235569",
+        "email": "gaston-gaitan@hotmail.com",
+        "document_picture": "doc_gaston_gaitan_3487235569.jpg"
+    },
+    {
+        "name": "Vicente pertossi",
+        "phone_number": "34872355629",
+        "email": "nicolas-capdepon@hotmail.com",
+        "document_picture": "doc_nico_capde_3487235569.jpg"
+    },
 ]
 
 @app.get("/show_pacients")
 async def show_pacients():
     return pacients
+
+@app.get("/filter_pacients")
+async def filter_pacients(key: str, value: str):
+    filtered_pacients = [pacient for pacient in pacients if value.lower() in pacient.get(key, "").lower()]
+    return filtered_pacients
