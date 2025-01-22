435271e44f5d09373c9a36745512090d7346fc9f
Gastón <gaston-gaitan@hotmail.com>
2025-01-22 16:52:29 -0300

    model config and filter by id

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
+from typing import Optional
 from fastapi import FastAPI
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, Field
 from pacient import Pacient
 
 app = FastAPI()
 
 class Pacient(BaseModel):
-    id: int = None
+    id: Optional[int] = Field(description="Unique identifier for the pacient", default=None)
     name: str
     phone_number: str
     email: EmailStr
     document_picture: str
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "name": "John Doe",
+                "phone_number": "123-456-7890",
+                "email": "john.doe@gmail.com",
+                "document_picture": "document_pacient_id_1.jpg"
+            }
+        }
+    }
 
 pacients = [
     Pacient(id=1, name="John Doe", phone_number="123-456-7890", email="johndoe@example.com", document_picture="path/to/document1.jpg"),
@@ -23,7 +35,16 @@ async def show_all_pacients():
 
 @app.get("/filter_pacients")
 async def filter_pacients(key: str, value: str):
-    filtered_pacients = [pacient for pacient in pacients if value.lower() in pacient.get(key, "").lower()]
+    filtered_pacients = []
+    for pacient in pacients:
+        if hasattr(pacient, key):
+            attribute_value = getattr(pacient, key)
+            if key == "id":
+                if attribute_value == int(value):
+                    filtered_pacients.append(pacient)
+            else:
+                if value.lower() in str(attribute_value).lower():
+                    filtered_pacients.append(pacient)
     return filtered_pacients
 
 @app.post("/create_pacient")
@@ -35,8 +56,8 @@ async def create_pacient(pacient: Pacient):
 @app.put("/update_pacient/{pacient_id}")
 async def update_pacient(pacient_id: int, key: str, value: str):
     for pacient in pacients:
-        if pacient["id"] == pacient_id:
-            pacient[key] = value
+        if hasattr(pacient, key):
+            setattr(pacient, key, value)
             return {"message": "Pacient updated successfully"}
     return {"error": "Pacient not found"}
 
