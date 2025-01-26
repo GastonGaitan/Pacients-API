2ceaaba2104a1bc39934cc809a85cafcdcadd2b1
Gastón <gaston-gaitan@hotmail.com>
2025-01-26 01:20:29 -0300

    creating docker container to run postgres database

--- database.py
+++ database.py
@@ -1,8 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-SQLALCHEMY_DATABASE = 'postgresql://postgres:41134345@localhost:5432/PacientsApi'
+SQLALCHEMY_DATABASE = os.getenv("SQL_CONNECTION")
 
 engine = create_engine(SQLALCHEMY_DATABASE)
 
