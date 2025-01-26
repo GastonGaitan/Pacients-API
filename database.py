87ef20c94311b9b5dd935b13bdc1186f5d17fc6a
Gastón <gaston-gaitan@hotmail.com>
2025-01-26 00:43:47 -0300

    adding connection to postgreSQL

--- database.py
+++ database.py
@@ -2,9 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE = 'sqlite:///./pacients.db'
+SQLALCHEMY_DATABASE = 'postgresql://postgres:41134345@localhost:5432/PacientsApi'
 
-engine = create_engine(SQLALCHEMY_DATABASE, connect_args={"check_same_thread": False})
+engine = create_engine(SQLALCHEMY_DATABASE)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
